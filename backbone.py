9470a719257adfb30b340aff70adcea32d37f020
neetmehta <neetmehta19@gmail.com>
2022-05-29 20:05:23 -0400

    model implemented

---
+++ backbone.py
@@ -0,0 +1,128 @@
+from turtle import forward
+from torchvision.models import resnet18, resnet34, resnet50, resnet101, resnet152 
+from torchvision.models import densenet121, densenet161, densenet169, densenet201, squeezenet1_0, squeezenet1_1
+import torch.nn as nn
+
+class ResNet(nn.Module):
+
+    def __init__(self, model_type, pretrained) -> None:
+        super(ResNet, self).__init__()
+
+        if model_type == 'resnet18':
+            self.model = resnet18(pretrained)
+
+        elif model_type == 'resnet34':
+            self.model = resnet34(pretrained)
+
+        elif model_type == 'resnet50':
+            self.model = resnet50(pretrained)
+
+        elif model_type == 'resnet101':
+            self.model = resnet101(pretrained)
+
+        elif model_type == 'resnet152':
+            self.model = resnet152(pretrained)
+
+        else:
+            print("unknown model")
+
+        del self.model._modules['avgpool']
+        del self.model._modules['fc']
+
+    def forward(self, x):
+
+        x = self.model._modules['conv1'](x)
+        x = self.model._modules['bn1'](x)
+        x = self.model._modules['relu'](x)
+        x = self.model._modules['maxpool'](x)
+        x = self.model._modules['layer1'](x)
+        x = self.model._modules['layer2'](x)
+        x = self.model._modules['layer3'](x)
+        x = self.model._modules['layer4'](x)
+        return x
+        
+
+
+class DenseNet(nn.Module):
+
+    def __init__(self, model_type, pretrained) -> None:
+        super(DenseNet, self).__init__()
+
+        if model_type == 'densenet121':
+            self.model = densenet121(pretrained)
+
+        elif model_type == 'densenet161':
+            self.model = densenet161(pretrained)
+
+        elif model_type == 'densenet169':
+            self.model = densenet169(pretrained)
+
+        elif model_type == 'densenet201':
+            self.model = densenet201(pretrained)
+
+        else:
+            print("unknown model")
+
+        del self.model._modules['classifier']
+
+    def forward(self, x):
+
+        return self.model._modules['features'](x)
+
+class SqueezeNet(nn.Module):
+
+    def __init__(self, model_type, pretrained) -> None:
+        super(SqueezeNet, self).__init__()
+
+        if model_type == 'squeezenet1_1':
+            self.model = squeezenet1_1(pretrained)
+
+        elif model_type == 'squeezenet1_0':
+            self.model = squeezenet1_0(pretrained)
+
+        else:
+            print("unknown model")
+
+
+    def forward(self, x):
+
+        return self.model(x)
+
+def model_factory(model_type, pretrained):
+    if model_type == 'densenet121':
+        model = DenseNet("densenet121", pretrained)
+
+    elif model_type == 'densenet161':
+        model = DenseNet("densenet161", pretrained)
+
+    elif model_type == 'densenet169':
+        model = DenseNet("densenet169", pretrained)
+
+    elif model_type == 'densenet201':
+        model = DenseNet("densenet201", pretrained)
+
+    elif model_type == 'resnet18':
+        model = ResNet("resnet18",pretrained)
+
+    elif model_type == 'resnet34':
+        model = ResNet("resnet34",pretrained)
+
+    elif model_type == 'resnet50':
+        model = ResNet("resnet50",pretrained)
+
+    elif model_type == 'resnet101':
+        model = ResNet("resnet101",pretrained)
+
+    elif model_type == 'resnet152':
+        model = ResNet("resnet152",pretrained)
+
+    elif model_type == 'squeezenet1_1':
+        model = SqueezeNet("squeezenet1_1",pretrained)
+
+    elif model_type == 'squeezenet1_0':
+        model = SqueezeNet("squeezenet1_0",pretrained)
+
+    else:
+        print("unknown model")
+
+    return model
---
+++ pspnet.py
@@ -0,0 +1,65 @@
+from turtle import forward
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+from backbone import model_factory
+
+class PSPModule(nn.Module):
+    def __init__(self,features, out_features=1024, sizes=(1, 2, 3, 6)) -> None:
+        super(PSPModule, self).__init__()
+        self.stages = []
+        self.stages = nn.ModuleList([self._make_stage(features, size) for size in sizes])
+        self.bottleneck = nn.Conv2d(features * (len(sizes) + 1), out_features, kernel_size=1)
+        self.relu = nn.ReLU()
+
+    def _make_stage(self, features, size):
+        prior = nn.AdaptiveAvgPool2d(output_size=(size, size))
+        conv = nn.Conv2d(features, features, kernel_size=1, bias=False)
+        return nn.Sequential(prior, conv)
+        
+
+    def forward(self, features):
+        h, w = features.size(2), features.size(3)
+        priors = [F.interpolate(input=stage(features), size=(h, w), mode='bilinear') for stage in self.stages] + [features]
+        bottle = self.bottleneck(torch.cat(priors, 1))
+        return self.relu(bottle)
+
+class PSPUpsample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.PReLU()
+        )
+
+    def forward(self, x):
+        h, w = 2 * x.size(2), 2 * x.size(3)
+        p = F.interpolate(input=x, size=(h, w), mode='bilinear')
+        return self.conv(p)
+
+
+class PSPNet(nn.Module):
+    def __init__(self, backbone_type='resnet34', pretrained=True, pool_scale=[1, 2, 3, 6], backbone_out_features=512, n_classes=20) -> None:
+        super(PSPNet,self).__init__()
+        self.backbone = model_factory(backbone_type, pretrained)
+        self.psp = PSPModule(backbone_out_features, 1024, pool_scale)
+        self.drop_1 = nn.Dropout2d(p=0.3)
+
+        self.up_1 = PSPUpsample(1024, 512)
+        self.up_2 = PSPUpsample(512, 256)
+        self.up_3 = PSPUpsample(256, 128)
+        self.up_4 = PSPUpsample(128, 64)
+        self.up_5 = PSPUpsample(64, 64)
+
+        self.drop_2 = nn.Dropout2d(p=0.15)
+        self.final = nn.Sequential(
+            nn.Conv2d(64, n_classes, kernel_size=1),
+            nn.LogSoftmax()
+        )
+        self.network = nn.Sequential(self.backbone, self.psp, self.drop_1, self.up_1, self.up_2, self.up_3, self.up_4, self.up_5, self.drop_2, self.final)
+
+    def forward(self, x):
+        return self.network(x)
+
