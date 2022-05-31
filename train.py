45cb0d279b5317525e28dd41119de6feb2bf4de2
neetmehta <neetmehta19@gmail.com>
2022-05-30 20:50:51 -0400

    resume ckpt

--- train.py
+++ train.py
@@ -13,11 +13,13 @@ torch.manual_seed(123)
 print('seed created')
 
 ROOT = "/Cityscapes"
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 LEARNING_RATE = 1e-4
-NUM_WORKERS = 0
+NUM_WORKERS = 2
 NUM_EPOCHS = 200
 CKPT_DIR = "ckpt"
+RESUME = True
+CKPT_PATH = "ckpt/pspnet_epoch_20.ckpt"
 os.makedirs(CKPT_DIR, exist_ok=True)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -28,13 +30,21 @@ val_data = Cityscapes(root=ROOT, set_type='val')
 
 train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
 val_loader = DataLoader(val_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKERS)
-
+start_epoch = 0
 model = PSPNet(backbone_type='resnet18').to(device)
+if RESUME:
+    state_dict = torch.load(CKPT_PATH)
+    model.load_state_dict(state_dict["model_state_dict"])
+    start_epoch = state_dict["epoch"] + 1
+    loss = state_dict['loss']
+    print(f"Starting training from epoch: {start_epoch-1} the loss was {loss}")
+
+
 criterion = torch.nn.BCEWithLogitsLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 print(f"Number of parameters = {sum(i.numel() for i in model.parameters())}")
 print('Starting training')
-for epoch in range(NUM_EPOCHS):
+for epoch in range(start_epoch, NUM_EPOCHS):
     loop = tqdm(train_loader)
     mean_loss = []
     for image, target, label in loop:
